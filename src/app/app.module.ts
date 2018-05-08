9d92e2d90df3a257c5286adaabba56aa7cc2339f
tjb1272 <tjb1272@gmail.com>
2018-05-07 19:44:01 -0700

    first upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { ListPage } from '../pages/list/list.component';
+import { AddPage } from '../pages/add/add.component';
+
+import { MyApp } from './app.component';
+import { NativeStorage } from '@ionic-native/native-storage';
+import { CommonService } from '../pages/service/common.service'
+
+@NgModule({
+  declarations: [
+    MyApp,
+    ListPage,
+    AddPage
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    ListPage,
+    AddPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    NativeStorage,
+    CommonService,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
